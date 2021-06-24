6ffaa79ded24a102ba7fded646fa0f7eb7a74173
Michael Romashov <mromashov@icloud.com>
2021-06-24 15:03:54 -0400

    Map completely fills parent container.

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,8 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, RefObject, useEffect, useRef, useState } from 'react';
 import { BingMap } from './BingMap';
 import { CanvasLayer, CanvasLayerController } from './CanvasLayer';
 import { useUpdate } from '../hooks';
+import map from '../../../../Pages/map';
 
 export type BingMapProps = {
     bingConfigFolder: string;
@@ -14,7 +15,7 @@ export type BingMapProps = {
 
 const DEFAULT_RANGE = 80;
 
-let rotation = 0;
+const rotation = 0;
 
 const colors = ['blue', 'red', 'green', 'yellow', 'purple'];
 
@@ -27,6 +28,8 @@ export const CanvasMap: FC<BingMapProps> = ({
 }) => {
     const [layerController, setLayerController] = useState<CanvasLayerController>();
 
+    const mapContainerRef = useRef<HTMLDivElement>(null);
+
     useUpdate((deltaTime: number) => {
         layerController?.use((canvas, context) => {
             rotation += 1000 * deltaTime;
@@ -35,17 +38,32 @@ export const CanvasMap: FC<BingMapProps> = ({
             context.clearRect(0, 0, canvas.getBoundingClientRect().width, canvas.getBoundingClientRect().height);
             // context.rotate(rotation * (Math.PI / 180));
             const colorIndex = Math.round(Math.random() * colors.length);
-            console.log(`color: ${colorIndex}`);
+            // console.log(`color: ${colorIndex}`);
             context.fillStyle = colors[colorIndex];
             context.fillRect(10, 10, 50, 50);
         });
     });
 
+    let mapSize;
+    if (mapContainerRef.current) {
+        mapSize = Math.hypot(mapContainerRef.current.clientWidth, mapContainerRef.current.clientHeight);
+    }
     return (
-        <div style={{ transform: `rotateZ(${rotation}deg)` }}>
-            <CanvasLayer onUpdatedDrawingCanvasController={setLayerController} />
-
-            <BingMap configFolder={bingConfigFolder} mapId={mapId} centerLla={centerLla} range={range} />
+        <div ref={mapContainerRef} style={{ position: 'relative', width: '100%', height: '100%' }}>
+            <div
+                style={{
+                    transform: `rotateZ(${rotation}deg) translateX(-50%) translateY(-50%)`,
+                    transformOrigin: '0 0',
+                    width: mapSize,
+                    height: mapSize,
+                    position: 'absolute',
+                    left: '50%',
+                    top: '50%',
+                }}
+            >
+                <CanvasLayer onUpdatedDrawingCanvasController={setLayerController} />
+                <BingMap configFolder={bingConfigFolder} mapId={mapId} centerLla={centerLla} range={range} />
+            </div>
         </div>
     );
 };
