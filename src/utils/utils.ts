f73b5fc22c4361068b828db7084989c930ce03ef
Michael Romashov <mromashov@icloud.com>
2021-07-21 17:27:24 -0400

    Converted utils to named functions.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,17 +1,20 @@
 import LatLon from 'geodesy/latlon-ellipsoidal-vincenty';
-import { GeoPath } from './GeoPath';
 
 /**
  * Converts a bearing to a cartesian angle in radians
  * @param bearing
  */
-export const bearingToRad = (bearing: number): number => ((450 - bearing) % 360) * (Math.PI / 180);
+export function bearingToRad(bearing: number): number {
+    return ((450 - bearing) % 360) * (Math.PI / 180);
+}
 
 /**
  * Converts from degrees to radians
  * @param degrees
  */
-export const degToRad = (degrees: number): number => (degrees * Math.PI) / 180;
+export function degToRad(degrees: number): number {
+    return (degrees * Math.PI) / 180;
+}
 
 /**
  * Given canvas context, converts a LatLon coordinate to the corresponding pixel location on the canvas
@@ -20,7 +23,7 @@ export const degToRad = (degrees: number): number => (degrees * Math.PI) / 180;
  * @param range
  * @param canvasWidth
  */
-export const latLonToPx = (latLon: LatLon, mapCenter: LatLon, range: number, canvasWidth: number): [number, number] => {
+export function latLonToPx(latLon: LatLon, mapCenter: LatLon, range: number, canvasWidth: number): [number, number] {
     const distanceToStart = (mapCenter.distanceTo(latLon)) / (3.02 * range);
     const angleToStart = bearingToRad(mapCenter.initialBearingTo(latLon)) || 0;
 
@@ -28,4 +31,4 @@ export const latLonToPx = (latLon: LatLon, mapCenter: LatLon, range: number, can
     const y = canvasWidth / 2 + distanceToStart * -Math.sin(angleToStart);
 
     return [x, y];
-};
+}
