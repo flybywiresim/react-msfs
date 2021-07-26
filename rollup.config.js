fe4967495c489dca8cc27fca5a878ecb76132152
benjozork <benjamin.dupont.984@gmail.com>
2021-07-25 23:41:31 -0400

    build: include type declarations

--- rollup.config.js
+++ rollup.config.js
@@ -8,6 +8,7 @@ import packageJson from './package.json';
 export default {
     input: 'src/index.ts',
     output: [
+        { dir: 'dist/' },
         {
             file: packageJson.main,
             format: 'cjs',
